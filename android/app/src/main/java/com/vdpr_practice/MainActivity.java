a75bcaf4f72dd86fb000bcb98d02f2c767895f6c
ph0ph0 <phillipalanlee@googlemail.com>
2020-01-23 09:11:28 +0000

    svg transformer not working

--- android/app/src/main/java/com/vdpr_practice/MainActivity.java
+++ android/app/src/main/java/com/vdpr_practice/MainActivity.java
@@ -4,7 +4,6 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-import com.horcrux.svg.SvgPackage
 
 public class MainActivity extends ReactActivity {
 
